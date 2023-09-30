c44bc64df23ec5796fc4601af6171db8410ddb7f
Pedro Garcia <garcia.pedro.a20@gmail.com>
2023-09-30 16:15:45 -0400

    initial commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use Inertia\Response;
+
+class DashboardController extends Controller
+{
+    function index(): Response
+    {
+        return Inertia::render('Dashboard');
+    }   
+}
