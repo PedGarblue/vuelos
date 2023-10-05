ace37e77713b7a0ea4324f99e38e86ea3d516b79
Pedro Garcia <garcia.pedro.a20@gmail.com>
2023-10-05 11:44:00 -0400

    style: removed bg on dark themes

--- resources/js/Layouts/GeneralLayout.vue
+++ resources/js/Layouts/GeneralLayout.vue
@@ -20,7 +20,6 @@
                 h-full
                 px-6 py-4
                 bg-white
-                dark:bg-gray-800
                 shadow-md
                 overflow-hidden
                 sm:rounded-lg
